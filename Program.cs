2372b3149ab5b45e56f8ab70988a4edd97467eef
Jamal Mavadat <jamal.mavadat@worldnomadsgroup.com>
2013-12-11 16:41:55 +1100

    Expect exactly 4 parameters

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.TeamFoundation.Build.Client;
 
 namespace CopyTfsBuildDefinitions
 {
@@ -10,6 +11,18 @@ namespace CopyTfsBuildDefinitions
     {
         static void Main(string[] args)
         {
+            if (args.Length != 4)
+            {
+                Console.WriteLine(@"Usage: CopyTfsBuildDefinitions <SourceTfsUri> <SourceProjectName> <TargetTfsUri> <TargetProjectName>
+e.g. CopyTfsBuildDefinitions http://tfs1:8080/tfs/default projectA http://tfs2:8080/tfs/default projectB");
+            }
+        }
+
+        static IBuildServer GetTfsBuildServer(string tfsUri)
+        {
+            var collection = new Microsoft.TeamFoundation.Client.TfsTeamProjectCollection(new Uri(tfsUri));
+            collection.EnsureAuthenticated();
+            return collection.GetService<IBuildServer>();
         }
     }
 }
