6af252e0d4f089504d738abcdb88a0ab45aab0b6
Jamal Mavadat <jamal.mavadat@worldnomadsgroup.com>
2013-12-11 17:04:04 +1100

    Copy build definitions by copying individual attributes

--- Program.cs
+++ Program.cs
@@ -13,16 +13,87 @@ namespace CopyTfsBuildDefinitions
         {
             if (args.Length != 4)
             {
-                Console.WriteLine(@"Usage: CopyTfsBuildDefinitions <SourceTfsUri> <SourceProjectName> <TargetTfsUri> <TargetProjectName>
-e.g. CopyTfsBuildDefinitions http://tfs1:8080/tfs/default projectA http://tfs2:8080/tfs/default projectB");
+                Console.WriteLine(@"Usage: CopyTfsBuildDefinitions <TfsCollectionUri> <SourceProjectName> <TargetProjectName>
+e.g. CopyTfsBuildDefinitions http://tfs1:8080/tfs/myCollection projectA projectB");
+            }
+
+            var tfsCollectionUri = args[0];
+            var sourceProjectName = args[1];
+            var targetProjectName = args[3];
+
+            var server = GetTfsBuildServer(tfsCollectionUri);
+
+            var sourceBuildDefinitions = server.QueryBuildDefinitions(sourceProjectName);
+            foreach (var sourceBuildDef in sourceBuildDefinitions)
+            {
+                var targetBuildDef = server.CreateBuildDefinition(targetProjectName);
+                Copy(sourceBuildDef, targetBuildDef);
+                targetBuildDef.Save();
             }
         }
 
-        static IBuildServer GetTfsBuildServer(string tfsUri)
+        static IBuildServer GetTfsBuildServer(string tfsCollectionUri)
         {
-            var collection = new Microsoft.TeamFoundation.Client.TfsTeamProjectCollection(new Uri(tfsUri));
+            var collection = new Microsoft.TeamFoundation.Client.TfsTeamProjectCollection(new Uri(tfsCollectionUri));
             collection.EnsureAuthenticated();
             return collection.GetService<IBuildServer>();
+        }
+
+        static void Copy(IBuildDefinition source, IBuildDefinition target)
+        {
+            target.BatchSize = source.BatchSize;
+            target.BuildController = source.BuildController;
+            target.ContinuousIntegrationType = source.ContinuousIntegrationType;
+            target.ContinuousIntegrationQuietPeriod = source.ContinuousIntegrationQuietPeriod;
+            target.DefaultDropLocation = source.DefaultDropLocation;
+            target.Description = source.Description;
+            target.Process = source.Process;
+            target.ProcessParameters = source.ProcessParameters;
+            target.QueueStatus = source.QueueStatus;
+            target.TriggerType = source.TriggerType;
+
+            CopySchedules(source, target);
+            CopyMappings(source, target);
+            CopyRetentionPolicies(source, target);
+        }
+
+        private static void CopyRetentionPolicies(IBuildDefinition source, IBuildDefinition target)
+        {
+            target.RetentionPolicyList.Clear();
+
+            foreach (var policy in source.RetentionPolicyList)
+            {
+                target.AddRetentionPolicy(
+                    policy.BuildReason,
+                    policy.BuildStatus,
+                    policy.NumberToKeep,
+                    policy.DeleteOptions
+                    );
+            }
+        }
+
+        private static void CopyMappings(IBuildDefinition source, IBuildDefinition target)
+        {
+            foreach (var mapping in source.Workspace.Mappings)
+            {
+                target.Workspace.AddMapping(
+                    mapping.ServerItem,
+                    mapping.LocalItem,
+                    mapping.MappingType,
+                    mapping.Depth
+                    );
+            }
+        }
+
+        private static void CopySchedules(IBuildDefinition source, IBuildDefinition target)
+        {
+            foreach (var schedule in source.Schedules)
+            {
+                var newSchedule = target.AddSchedule();
+                newSchedule.DaysToBuild = schedule.DaysToBuild;
+                newSchedule.StartTime = schedule.StartTime;
+                newSchedule.TimeZone = schedule.TimeZone;
+            }
         }
     }
 }
